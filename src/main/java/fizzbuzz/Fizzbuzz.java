6a08bcda7499c82fdc1da6011e248a0dbb33e992
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-11 20:37:47 -0400

    refactorizando la clase Fizzbuzz

--- src/main/java/fizzbuzz/Fizzbuzz.java
+++ src/main/java/fizzbuzz/Fizzbuzz.java
@@ -4,15 +4,31 @@ public class Fizzbuzz {
 	
 	public String fizzbuzz(int numero){
 		
-		if (numero % 3 == 0 && numero % 5 == 0){
+		if (esMultiploDeTres(numero) && esMultiploDeCinco(numero)){
 			return "FizzBuzz";
 		}
-		if (numero % 3 == 0){
+		if (esMultiploDeTres(numero)){
 			return "Fizz";
 		}
-		if (numero % 5 == 0){
+		if (esMultiploDeCinco(numero)){
 			return "Buzz";
 		}
 			return String.valueOf(numero);
+	}
+	
+	private boolean esMultiploDeTres(int numero){
+		boolean resp = false;
+		if (numero % 3 == 0){
+			resp = true;
+		}
+			return resp;
+	}
+	
+	private boolean esMultiploDeCinco(int numero){
+		boolean resp = false;
+		if (numero % 5 == 0){
+			resp = true;
+		}
+			return resp;
 	}
 }
