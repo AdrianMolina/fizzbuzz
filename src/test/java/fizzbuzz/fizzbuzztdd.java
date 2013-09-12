f32faf1e05202e56d776cd0c181cf3538866d133
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-11 20:32:25 -0400

    refactorizando pruebas de fizzbuzz

--- src/test/java/fizzbuzz/fizzbuzztdd.java
+++ src/test/java/fizzbuzz/fizzbuzztdd.java
@@ -5,28 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class fizzbuzztdd {
+	
+	Fizzbuzz fb = new Fizzbuzz();
 
 	@Test
 	public void retornarUnNumero(){
-		Fizzbuzz fb = new Fizzbuzz();
 		assertEquals("1",fb.fizzbuzz(1));
 	}
 	
 	@Test
 	public void retornarFizzCuandoNumeroSeaMultiploDeTres(){
-		Fizzbuzz fb = new Fizzbuzz();
 		assertEquals("Fizz",fb.fizzbuzz(3));
 	}
 	
 	@Test
 	public void retornarBuzzCuandoNumeroSeaMultiploDeCinco(){
-		Fizzbuzz fb = new Fizzbuzz();
 		assertEquals("Buzz",fb.fizzbuzz(5));
 	}
 	
 	@Test
 	public void retornarFizzBuzzCuandoNumeroSeaMultiploDeTresyCinco(){
-		Fizzbuzz fb = new Fizzbuzz();
 		assertEquals("FizzBuzz",fb.fizzbuzz(15));
 	}
 
