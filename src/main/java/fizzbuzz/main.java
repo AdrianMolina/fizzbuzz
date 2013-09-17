a07f9eb79d38582ce8cf0d3b34a8002c71312dc4
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-16 20:36:30 -0400

    primer commit al repositorio remoto

--- src/main/java/fizzbuzz/main.java
+++ src/main/java/fizzbuzz/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		Fizzbuzz fb = new Fizzbuzz();
 		
-		for(int i = 1; i <= 30; i++){
+		for(int i = 1; i <= 100; i++){
 			System.out.println(fb.fizzbuzz(i));
 		}
 
