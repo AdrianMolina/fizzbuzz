7c8c504b85eddc8f2821a08841febc73c6e019ca
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-21 19:40:23 -0400

    corrigiendo errores en pom.xml

--- src/main/java/fizzbuzz/fizzbuzzServlet.java
+++ src/main/java/fizzbuzz/fizzbuzzServlet.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
 
 public class fizzbuzzServlet extends HttpServlet {
 	@Override
@@ -15,15 +13,5 @@ public class fizzbuzzServlet extends HttpServlet {
 		String hasta = request.getParameter("hasta");
 		response.getWriter().println(fb.imprimir(Integer.parseInt(hasta)));
 		//response.getWriter().println("Se recibio el parametro: " + hasta);
-	}
-	
-	public static void main(String[] args) throws Exception{
-		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-		context.setContextPath("/");
-		server.setHandler(context);
-		context.addServlet(new ServletHolder(new fizzbuzzServlet()), "/");
-		server.start();
-		server.join();
 	}
 }
