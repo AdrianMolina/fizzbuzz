15312934e6356ce9d845762c3e88d362b3a98361
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-21 17:48:53 -0400

    logica fizzbuzz funcionando de manera local

--- src/main/java/fizzbuzz/fizzbuzzServlet.java
+++ src/main/java/fizzbuzz/fizzbuzzServlet.java
@@ -11,9 +11,9 @@ public class fizzbuzzServlet extends HttpServlet {
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		Fizzbuzz fb = new Fizzbuzz();
 		String hasta = request.getParameter("hasta");
-		response.getWriter().println("Se recibio el parametro: " + hasta);
+		response.getWriter().println(fb.imprimir(Integer.parseInt(hasta)));
+		//response.getWriter().println("Se recibio el parametro: " + hasta);
 	}
-	
-
 }
