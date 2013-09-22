1a003b66d1474358357a46d68d096c6724decbd1
AdrianMolina <adrian6_6_6@hotmail.com>
2013-09-21 20:06:56 -0400

    agrgando main a fizzbuzzServlet y algunas dependencias a pom.xml

--- src/main/java/fizzbuzz/fizzbuzzServlet.java
+++ src/main/java/fizzbuzz/fizzbuzzServlet.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.*;
 
 public class fizzbuzzServlet extends HttpServlet {
 	@Override
@@ -14,4 +16,14 @@ public class fizzbuzzServlet extends HttpServlet {
 		response.getWriter().println(fb.imprimir(Integer.parseInt(hasta)));
 		//response.getWriter().println("Se recibio el parametro: " + hasta);
 	}
+	
+	public static void main(String[] args) throws Exception{
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.setContextPath("/");
+        server.setHandler(context);
+        context.addServlet(new ServletHolder(new fizzbuzzServlet()),"/*");
+        server.start();
+        server.join();   
+    }
 }
